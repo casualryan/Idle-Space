778c4d66cf4ae85bad5e42a0533b10ff54cbaf49
casualryan <154554093+casualryan@users.noreply.github.com>
2026-08-08 11:03:56 -0400

    Add material icon experiment

--- src/items/materials/toxicResidue.js
+++ src/items/materials/toxicResidue.js
@@ -1,5 +1,5 @@
 export default {
   name: 'Toxic Residue', type: 'Material', slot: 'material', stackable: true,
-  icon: 'icons/default-icon.png', isDisassembleable: false,
+  icon: 'icons/toxic_residue.png', isDisassembleable: false,
   description: 'Concentrated chemical residue recovered from toxic equipment.'
 };
